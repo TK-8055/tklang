fc7f35ce161c7e695b1e2dffbfd83cb857877c30
TK <dharaneeshtk8055@gmail.com>
2026-03-06 23:18:22 +0530

    Add arithmetic operators support

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,27 +3,35 @@ class TKInterpreter:
     def __init__(self):
         self.variables = {}
 
-    def evaluate(self, node):
+    def evaluate(self,node):
 
-        if node[0] == 'number':
+        if node[0]=='number':
             return node[1]
 
-        if node[0] == 'string':
+        if node[0]=='string':
             return node[1].strip('"')
 
-        if node[0] == 'var':
-            return self.variables.get(node[1], 0)
+        if node[0]=='var':
+            return self.variables.get(node[1],0)
 
-        if node[0] == 'add':
+        if node[0]=='add':
             return self.evaluate(node[1]) + self.evaluate(node[2])
 
-    def execute(self, tree):
+        if node[0]=='sub':
+            return self.evaluate(node[1]) - self.evaluate(node[2])
 
-        if tree[0] == 'assign':
+        if node[0]=='mul':
+            return self.evaluate(node[1]) * self.evaluate(node[2])
+
+        if node[0]=='div':
+            return self.evaluate(node[1]) / self.evaluate(node[2])
+
+    def execute(self,tree):
+
+        if tree[0]=='assign':
             name = tree[1]
             value = self.evaluate(tree[2])
             self.variables[name] = value
 
-        elif tree[0] == 'print':
-            value = self.evaluate(tree[1])
-            print(value)
+        elif tree[0]=='print':
+            print(self.evaluate(tree[1]))
